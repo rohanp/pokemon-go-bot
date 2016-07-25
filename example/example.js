4e6cc356fdf3a7f29154e709b3b9c12fa79c6a13
stoffern <kristoffer.klevenberg@gmail.com>
2016-07-25 16:14:58 +0200

    update example

--- example/example.js
+++ example/example.js
@@ -3,36 +3,60 @@ import PokeAPI from '../src'
 
 env(__dirname + '/.env');
 
-//Set environment variables or replace placeholder text
-var location = {
-    type: 'name',
-    name: process.env.PGO_LOCATION || 'Times Square'
-};
-
 var username = process.env.PGO_USERNAME || 'USER'
 var password = process.env.PGO_PASSWORD || 'PASS'
 var provider = process.env.PGO_PROVIDER || 'ptc'
+var lat = process.env.LATITUDE || 40.759211
+var lng = process.env.LONGITUDE || -73.984472
+
 
 const Poke = new PokeAPI()
 
 async function init() {
 
+  //set player location
   Poke.player.location = {
-    latitude: 55.70000932453345,
-    longitude: 12.524757385253906,
+    latitude: parseFloat(lat),
+    longitude: parseFloat(lng),
   }
 
+  //login
   const api = await Poke.login(username, password, provider)
 
-  let res = await Poke.GetPlayer()
-  console.log(res)
-  let res2 = await Poke.GetInventory()
-  console.log(res2)
-  let res3 = await Poke.GetMapObjects()
-  console.log(res3)
+  // just update the profile...
+  let player = await Poke.GetPlayer()
 
-  //walk a little..
-  Poke.player.WalkAround()
+  // get map objects..
+  while( true ){
+    let cells = await Poke.GetMapObjects()
+    for(let cell of cells) {
+      // catchable pokemons from here?
+      if (cell.catchable_pokemons.length > 0){
+        cell.catchable_pokemons.map(pokemon => {
+          pokemon.encounterAndCatch()
+        })
+      }
+
+      // wild pokemons
+      if (cell.wild_pokemons.length > 0){
+        // we have wild pokemons, you cannot catch these..
+      }
+
+      // forts
+      if (cell.forts.length > 0){
+        // forts..
+        for (let fort of cell.forts){
+          if (fort.isCheckpoint)
+            fort.search()
+        }
+      }
+    }
+    
+    //just walk a little (1 - 15 meters..)
+    await Poke.player.walkAround()
+
+    await new Promise(resolve => setTimeout(resolve, 3000))
+  }
 }
 
 init().catch(console.log)
--- example/robot.js
+++ example/robot.js
@@ -1,16 +1,14 @@
 import env from 'node-env-file'
+import geolib from 'geolib'
 import PokeAPI from '../src'
 
-import geolib from 'geolib'
-
 env(__dirname + '/.env');
-
 
 var username = process.env.PGO_USERNAME || 'USER'
 var password = process.env.PGO_PASSWORD || 'PASS'
 var provider = process.env.PGO_PROVIDER || 'ptc'
-var latitude = process.env.PGO_LATITUDE || 40.759211
-var longitude = process.env.PGO_LONGITUDE || -73.984472
+var lat = process.env.LATITUDE || 40.759211
+var lng = process.env.LONGITUDE || -73.984472
 
 //Set environment variables or replace placeholder text
 
@@ -20,44 +18,43 @@ async function init() {
   //yep, we do need to login..
 
   Poke.player.location = {
-    latitude: 55.70000932453345,
-    longitude: 12.524757385253906,
+    latitude: parseFloat(lat),
+    longitude: parseFloat(lng),
   }
 
   const api = await Poke.login(username, password, provider)
 
-
   // just update the profile...
   let player = await Poke.GetPlayer()
-  let player = await Poke.GetInvetory()
+  let inventory = await Poke.GetInvetory()
 
-  // // get map objects..
-  // while( true ){
-  //   let cells = await Poke.GetMapObjects()
-  //   for(let cell of cells) {
-  //     // catchable pokemons from here?
-  //     if (cell.catchable_pokemons.length > 0){
-  //       cell.catchable_pokemons.map(pokemon => {
-  //         pokemon.encounterAndCatch()
-  //       })
-  //     }
+  // get map objects..
+  while( true ){
+    let cells = await Poke.GetMapObjects()
+    for(let cell of cells) {
+      // catchable pokemons from here?
+      if (cell.catchable_pokemons.length > 0){
+        cell.catchable_pokemons.map(pokemon => {
+          pokemon.encounterAndCatch()
+        })
+      }
 
-  //     // wild pokemons
-  //     if (cell.wild_pokemons.length > 0){
-  //       // we have wild pokemons
-  //     }
+      // wild pokemons
+      if (cell.wild_pokemons.length > 0){
+        // we have wild pokemons
+      }
 
-  //     // forts
-  //     if (cell.forts.length > 0){
-  //       // we have wild pokemons
-  //     }
+      // forts
+      if (cell.forts.length > 0){
+        // we have wild pokemons
+      }
 
-  //     //Done...
-  //     //TODO: We need to move.. like a human..!
-  //   }
-  //   await Poke.player.walkAround()
-  //   await new Promise(resolve => setTimeout(resolve, 3000))
-  // }
+      //Done...
+      //TODO: We need to move.. like a human..!
+    }
+    await Poke.player.walkAround()
+    await new Promise(resolve => setTimeout(resolve, 3000))
+  }
 }
 
 init().catch(console.log)
