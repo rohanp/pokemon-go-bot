7c58669f671ce7ce6a2d05ca443ef92c6e16ae08
stoffern <kristoffer.klevenberg@gmail.com>
2016-07-25 10:20:34 +0200

    Fort functions.. (Checkpoint and Gym)

--- src/Fort.js
+++ src/Fort.js
@@ -1,59 +1,96 @@
-import API from './API'
+
+/**
+ * Called if a parameter is missing and
+ * the default value is evaluated.
+ */
+function mandatory() {
+    throw new Error('Missing parameter');
+}
+
 
 class Fort{
-  constructor(props) {
+  constructor(props, parent) {
     Object.assign(this, props)
     delete this.id
-    this.api = new API()
+    this.parent = parent
   }
 
-  recallPokemon(fort_id, pokemon_id = mandatory()) {
-    return this.Call([{
-      request: 'FORT_RECALL_POKEMON',
-      message: {
-        fort_id: fort_id,
-        pokemon_id: pokemon_id,
-        player_latitude: this.player.playerInfo.latitude,
-        player_longitude: this.player.playerInfo.longitude,
-      }
-    }])
+  isCheckpoint(){
+    if (this.type == 1){
+      return true
+    }else{
+      return false
+    }
   }
 
-  deployPokemon(fort_id, pokemon_id = mandatory()) {
-    return this.Call([{
-      request: 'FORT_DEPLOY_POKEMON',
-      message: {
-        fort_id: fort_id,
-        pokemon_id: pokemon_id,
-        player_latitude: this.player.playerInfo.latitude,
-        player_longitude: this.player.playerInfo.longitude,
-      }
-    }])
+  isGym(){
+    if (this.type == 0){
+      return true
+    }else{
+      return false
+    }
   }
 
-  details(fort = mandatory()) {
-    return this.Call([{
-      request: 'FORT_DETAILS',
-      message: {
-        fort_id: fort.fort_id,
-        latitude: fort.latitude,
-        longitude: fort.longitude,
-      }
-    }])
-  }
-
-  search(fort = mandatory()) {
+  search() {
     return this.Call([{
       request: 'FORT_SEARCH',
       message: {
-        fort_id: fort.fort_id,
+        fort_id: this.fort.fort_id,
         player_latitude: this.player.playerInfo.latitude,
         player_longitude: this.player.playerInfo.longitude,
-        fort_latitude: fort.fort_latitude,
-        fort_longitude: fort.fort_longitude
+        fort_latitude: this.fort.fort_latitude,
+        fort_longitude: this.fort.fort_longitude
       }
     }])
   }
+
+  recallPokemon(pokemon_id = mandatory()) {
+    return this.Call([{
+      request: 'FORT_RECALL_POKEMON',
+      message: {
+        fort_id: this.fort_id,
+        pokemon_id: pokemon_id,
+        player_latitude: this.parent.player.playerInfo.latitude,
+        player_longitude: this.parent.player.playerInfo.longitude,
+      }
+    }])
+  }
+
+  deployPokemon(pokemon_id = mandatory()) {
+    return this.Call([{
+      request: 'FORT_DEPLOY_POKEMON',
+      message: {
+        fort_id: this.fort_id,
+        pokemon_id: pokemon_id,
+        player_latitude: this.player.playerInfo.latitude,
+        player_longitude: this.player.playerInfo.longitude,
+      }
+    }])
+  }
+
+  details() {
+    return this.Call([{
+      request: 'FORT_DETAILS',
+      message: {
+        fort_id: this.fort.fort_id,
+        latitude: this.fort.latitude,
+        longitude: this.fort.longitude,
+      }
+    }])
+  }
+  addModifier(item_id){
+    return this.Call([{
+      request: 'FORT_DETAILS',
+      message: {
+        modifier_type: item_id,
+        fort_id: this.fort.fort_id,
+        player_latitude: this.parent.player.playerInfo.latitude,
+        player_longitude: this.parent.player.playerInfo.longitude,
+      }
+    }])
+
+  }
+
 }
 export default Fort
 
